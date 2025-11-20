f9b2e886d29e1ec810b5ca8a79483de54032a530
FebbAdityaN <71268462+FebbAdityaN@users.noreply.github.com>
2025-11-20 12:10:47 +0700

    Add files via upload

---
+++ src/impostor/guide.js
@@ -0,0 +1,33 @@
+export const guide = `↱ \`IMPOSTOR GAME\` ↲
+
+*PANDUAN / GUIDE*
+
+Semua perintah dibawah tanpa menggunakan < >
+
+             *⇃⇃ Diluar Room / Lobby ⇂⇂*
+
+1. Kalian harus mendaftar terlebih dahulu, dengan menjalankan perintah *#daftar <nama>*
+2. Jika kalian ingin membuat Room sendiri, kalian harus menjalankan perintah *#buatroom <nama room>*
+3. Jika teman kalian sudah ada yang membuat room dan kalian ingin memasuki room tersebut, kalian tinggal menjalankan perintah *#join <nama room>*
+4. Jika kalian tidak mengetahui nama roomnya, kalian tinggal menjalankan perintah *#room* untuk melihat list room yang ada.
+
+              *⇃⇃ Didalam Room / Grup ⇂⇂*
+
+1. *#start* adalah perintah untuk memulai permainan, permainan dapat dimulai ketika didalam room/grup minimal ada 4 pemain dan maksimal 5 pemain.
+2. *#leave* adalah perintah ketika kamu ingin keluar dari room/grup, sebelum kamu keluar dari grup, diusahakan untuk menjalankan perintah ini terlebih dahulu, agar database dapat sinkron dengan permainan.
+3. *#delete* adalah perintah penghapusan sesi permainan, ketika kamu menjalankan permainan, semua sesi akan dihapus dan Bot akan keluar dari room/grup.
+
+              *⇃⇃ Sesi diPrivate Chat Bot ⇂⇂*
+
+1. *#clue <kata>* adalah perintah untuk memberikan clue dari kata kunci yang telah diberikan.
+2. *#vote <62 nohp>* adalah perintah untuk melakukan voting, perintah ini dapat dijalankan ketika sesi voting dimulai.
+3. *#listplayer* adalah perintah untuk melihat list pemain yang ada didalam room yang kamu mainkan, ini juga untuk melihat nomor hp para pemain untuk melakukan voting.
+
+                *⇃⇃ CARA BERMAIN ⇂⇂*
+
+1. Ketika perintah *#start* dijalankan, bot akan mengacak siapa yang akan menjadi impostor.
+2. Kemudian sesi memberikan clue juga dimulai, bot akan mengarahkan didalam grup, siapa yang akan memberikan clue terlebih dahulu (memberikan clue secara bergantian).
+3. Ketika semua pemain telah memberikan clue, sesi voting dimulai. Kamu dapat melakukan voting kepada siapapun yang kamu curigai sebagai Impostor (tidak dapat melakukan voting pada diri sendiri).
+4. Kemudian jika semua pemain telah melakukan voting, Bot akan menghitung mayoritas suara, Jika mayoritas benar menebak Impostornya, para *Manusia* (non impostor) memenangkan permainannya, jika suara seimbang maka Impostor yang memenangkan permainan karena tidak dapat menebaknya, begitu pula jika kalian salah menebak Impostornya.
+
+\`WhatsApp Bot created by @febbyadityan\``
--- src/impostor/index.js
+++ src/impostor/index.js
@@ -1,1 +1,14 @@
-
+export {
+	newPlayers,
+	checkUser,
+	newRoom,
+	checkRoom,
+	joinRoom,
+	leaveRoom,
+	deleteRoom,
+	startGame,
+	stopGame,
+	clue,
+	voting
+} from './lib/function.js';
+export { guide } from './guide.js';
